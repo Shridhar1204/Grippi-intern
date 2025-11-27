c2e12f05d6b232de5ba9f9399076536e8e7ac31b
Shridhar1204 <sharmashridhar1204@gmail.com>
2025-11-27 22:31:36 +0530

    CORS and schema naming error fix

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
+
 from typing import List, Optional
 
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from database import Base, engine, SessionLocal
 from models import Campaign
-from schemas import CampaignResponse
+from schemas import CampaignsResponse
 from sample import sample_data
 
 Base.metadata.create_all(bind=engine)
@@ -13,7 +15,22 @@ Base.metadata.create_all(bind=engine)
 
 sample_data()
 
-app = FastAPI(title="Grippi Campaign API")
+app = FastAPI(title="Grippi Campaign Analytics API")
+
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def get_db():
