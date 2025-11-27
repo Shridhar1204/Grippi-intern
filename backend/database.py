e734388f814230b4b17cb14099c1e043ac54f019
Shridhar1204 <sharmashridhar1204@gmail.com>
2025-11-27 21:14:03 +0530

    db created

---
+++ backend/database.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+DEFAULT_LOCAL_DB_URL = "postgresql+psycopg2://postgres:shridhar@localhost:5432/grippi_db"
+
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,  
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+Base = declarative_base()
