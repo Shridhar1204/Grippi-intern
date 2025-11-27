5a55dc2bb8f3435b1722e64b10d65fa6d7aace2c
Shridhar1204 <sharmashridhar1204@gmail.com>
2025-11-27 22:26:59 +0530

    DATABASE URL Error and Requirement error fix

--- backend/database.py
+++ backend/database.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DEFAULT_LOCAL_DB_URL = "postgresql+psycopg2://postgres:shridhar@localhost:5432/grippi_db"
 
+DATABASE_URL = os.getenv("DATABASE_URL", DEFAULT_LOCAL_DB_URL)
+
 
 engine = create_engine(
     DATABASE_URL,
